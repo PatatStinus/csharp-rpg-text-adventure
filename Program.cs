73684d813fd0e19a322cfbcecc7b463853ebb902
PatatStinus <71143317+PatatStinus@users.noreply.github.com>
2021-09-29 15:41:29 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Threading;
 namespace RPGTextBasedAdventure
 {
     class Program
@@ -7,6 +7,25 @@ namespace RPGTextBasedAdventure
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            ShowSimplePercentage();
+
+            Console.ReadKey();
+        }
+
+        static void ShowSimplePercentage()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.Write($"\rProgress: {i}%   ");
+                if(i == 99)
+                    Thread.Sleep(10000);
+                else
+                    Thread.Sleep(25);
+            }
+
+            Console.Write("\rDone!          ");
         }
     }
 }
