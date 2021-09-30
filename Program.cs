3cd9e2e6725fd52543bae62c06e64df4acbde046
PatatStinus <71143317+PatatStinus@users.noreply.github.com>
2021-09-30 12:05:18 +0200

    Concept + name input made

--- Program.cs
+++ Program.cs
@@ -4,28 +4,43 @@ namespace RPGTextBasedAdventure
 {
     class Program
     {
+        private static string name = "";
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome! Press Enter to load game.");
             Console.ReadKey();
 
             ShowSimplePercentage();
 
-            Console.ReadKey();
+            GetName();
         }
 
         static void ShowSimplePercentage()
         {
             for (int i = 0; i <= 100; i++)
             {
-                Console.Write($"\rProgress: {i}%   ");
-                if(i == 99)
-                    Thread.Sleep(10000);
+                Console.Write($"\rLoading: {i}%");
+                if (i == 98 || i == 99)
+                    Thread.Sleep(5000);
+                else if (i >= 64 && i <= 72)
+                    Thread.Sleep(1000);
                 else
                     Thread.Sleep(25);
             }
 
-            Console.Write("\rDone!          ");
+            Console.Write("\rLoading complete! Welcome to the game!");
+            Console.WriteLine("\nFirst off, please enter your name!");
+        }
+
+        static void GetName()
+        {
+            name = Console.ReadLine();
+            if(name == "")
+            {
+                Console.WriteLine("Please write a correct name.");
+                GetName();
+            }
         }
     }
 }
