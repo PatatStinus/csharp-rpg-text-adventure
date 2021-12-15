cbdc6223c945ac9b4785347e4adae72c0e0c2eab
PatatStinus <71143317+PatatStinus@users.noreply.github.com>
2021-12-15 14:36:16 +0100

    Safe Sysrtrm

--- Program.cs
+++ Program.cs
@@ -1,45 +1,213 @@
 ﻿using System;
 using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
 namespace RPGTextBasedAdventure
 {
     class Program
     {
-        private static string name = "";
-        
+        private static List<string> fileNames = new List<string>();
+        private static int chosenRace = 0;
+        private static string userInput;
+
+        static bool ConsoleEventCallback(int eventType)
+        {
+            if (eventType == 2)
+            {
+                Console.WriteLine("Closing window. Please wait...");
+                Console.ReadLine();
+            }
+            return false;
+        }
+
+        static ConsoleEventDelegate handler;
+        private delegate bool ConsoleEventDelegate(int eventType);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome! Press Enter to load game.");
-            Console.ReadKey();
+            handler = new ConsoleEventDelegate(ConsoleEventCallback);
+            SetConsoleCtrlHandler(handler, true);
 
-            ShowSimplePercentage();
+            Console.WriteLine("Welcome! Press Enter to start the game!");
 
-            GetName();
+
+            userInput = Console.ReadLine();
+
+            GetSaveFiles();
+
+            Console.WriteLine("Please choose a save file!");
+
+            SaveFiles();
+
+            ChooseFile();
         }
 
-        static void ShowSimplePercentage()
+        static void GetSaveFiles()
         {
-            for (int i = 0; i <= 100; i++)
+            if (!Directory.Exists("savefiles"))
+                Directory.CreateDirectory("savefiles");
+
+            string[] files = Directory.GetFiles("savefiles", "*.*", SearchOption.AllDirectories);
+            foreach(string file in files)
+                fileNames.Add(Path.GetFileName(file));
+        }
+
+        static void SaveFiles()
+        {
+            StreamReader saveFiles;
+            for (int i = 0; i < fileNames.Count; i++)
             {
-                Console.Write($"\rLoading: {i}%");
-                if (i == 98 || i == 99)
-                    Thread.Sleep(5000);
-                else if (i >= 64 && i <= 72)
-                    Thread.Sleep(1000);
-                else
-                    Thread.Sleep(25);
+                Console.WriteLine(i + 1 + ".");
+                saveFiles = File.OpenText("savefiles/" + fileNames[i]);
+                string line = saveFiles.ReadLine();
+                int numbLine = 0;
+                while(line != null)
+                {
+                    switch(numbLine)
+                    {
+                        case 0:
+                            Console.WriteLine(" Name = " + line);
+                            break;
+                        case 1:
+                            Console.WriteLine(" Level = " + line);
+                            break;
+                        case 2:
+                            Console.WriteLine(" XP = " + line);
+                            break;
+                        case 3:
+                            switch(Int16.Parse(line))
+                            {
+                                case 1:
+                                    Console.WriteLine(" Race = Witch");
+                                    break;
+                                case 2:
+                                    Console.WriteLine(" Race = Golem");
+                                    break;
+                                case 3:
+                                    Console.WriteLine(" Race = ManBird");
+                                    break;
+                                case 4:
+                                    Console.WriteLine(" Race = Human");
+                                    break;
+                                case 5:
+                                    Console.WriteLine(" Race = Fire Spirit");
+                                    break;
+                                case 6:
+                                    Console.WriteLine(" Race = Swift");
+                                    break;
+                            }
+                            break;
+                    }
+                    line = saveFiles.ReadLine();
+                    numbLine++;
+                }
+                saveFiles.Close();
             }
 
-            Console.Write("\rLoading complete! Welcome to the game!");
-            Console.WriteLine("\nFirst off, please enter your name!");
+            Console.WriteLine((fileNames.Count + 1) + ".");
+            Console.WriteLine(" New Save file");
+            Console.WriteLine();
         }
 
-        static void GetName()
+        static void ChooseFile()
         {
-            name = Console.ReadLine();
-            if(name == "")
+            string typedFile = Console.ReadLine();
+
+            if(Int16.TryParse(typedFile, out short j) && j <= fileNames.Count + 1 )
             {
-                Console.WriteLine("Please write a correct name.");
-                GetName();
+                if (j == fileNames.Count + 1) 
+                {
+                    Console.WriteLine("Please enter a name for your save file!");
+                    fileNames.Add(Console.ReadLine() + ".txt");
+                    StreamWriter newFile = File.CreateText("savefiles/" + fileNames[j - 1]);
+                    Console.WriteLine("Enter your character name");
+                    newFile.WriteLine(Console.ReadLine());
+                    newFile.WriteLine("1"); //LVL
+                    newFile.WriteLine("0"); //XP
+                    Console.WriteLine("Please Choose 1 of the 6 races you want your character to be:");
+                    ChooseRace();
+                    newFile.WriteLine(chosenRace); //Race
+                    newFile.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please type the number of the save file you want to select.");
+                ChooseFile();
+            }
+        }
+
+        static void ChooseRace()
+        {
+            Console.WriteLine("1. \n Witch");
+            Console.WriteLine("2. \n Golem");
+            Console.WriteLine("3. \n ManBird");
+            Console.WriteLine("4. \n Human");
+            Console.WriteLine("5. \n Fire Spirit");
+            Console.WriteLine("6. \n Swift");
+            Console.WriteLine("Type [Race Number] + help to learn more about the race. Example: 3help");
+            RaceInput();
+        }
+
+        static void RaceInput()
+        {
+            while(chosenRace == 0)
+            {
+                userInput = Console.ReadLine();
+                if (String.Compare(userInput, "1help", true) == 0)
+                    Console.WriteLine("The Witch has magic powers that can shoot long and short range and also has good defense. \nCounters: ManBird + Fire Spirit");
+                else if (String.Compare(userInput, "2help", true) == 0)
+                    Console.WriteLine("The Golem is immune to magic powers, does really hard hitting melee attacks and has good defense but is very slow. \nCounters: Witch + Human");
+                else if (String.Compare(userInput, "3help", true) == 0)
+                    Console.WriteLine("The ManBird is the only race with flight, can dodge melee attacks and can reach the Cloud World that some other races can't. \nCounters: Golem + Swift");
+                else if (String.Compare(userInput, "4help", true) == 0)
+                    Console.WriteLine("The Human is the basic race, medium stats on everything. \nCounters: Fire Spirit + ManBird");
+                else if (String.Compare(userInput, "5help", true) == 0)
+                    Console.WriteLine("The Fire Spirit is really fast and does a lot of damage but all stats decrease when exposed to light. \nCounters: Human + Swift");
+                else if (String.Compare(userInput, "6help", true) == 0)
+                    Console.WriteLine("The Swift is the fastest race and can dodge some attacks randomly but has low damage and low defense. \nCounters: Witch + Golem");
+                else if (String.Compare(userInput, "1", true) == 0)
+                {
+                    Console.WriteLine("You have chosen to become the Witch!");
+                    chosenRace = Int16.Parse(userInput);
+                    break;
+                }
+                else if (String.Compare(userInput, "2", true) == 0)
+                {
+                    Console.WriteLine("You have chosen to become the Golem!");
+                    chosenRace = Int16.Parse(userInput);
+                    break;
+                }
+                else if (String.Compare(userInput, "3", true) == 0)
+                {
+                    Console.WriteLine("You have chosen to become the ManBird!");
+                    chosenRace = Int16.Parse(userInput);
+                    break;
+                }
+                else if (String.Compare(userInput, "4", true) == 0)
+                {
+                    Console.WriteLine("You have chosen to become the Human!");
+                    chosenRace = Int16.Parse(userInput);
+                    break;
+                }
+                else if (String.Compare(userInput, "5", true) == 0)
+                {
+                    Console.WriteLine("You have chosen to become the Fire Spirit!");
+                    chosenRace = Int16.Parse(userInput);
+                    break;
+                }
+                else if (String.Compare(userInput, "6", true) == 0)
+                {
+                    Console.WriteLine("You have chosen to become the Swift!");
+                    chosenRace = Int16.Parse(userInput);
+                    break;
+                }
+                else
+                    Console.WriteLine("Please choose a race you would like to be!");
             }
         }
     }
---
+++ TheTower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGTextBasedAdventure
+{
+    class TheTower
+    {
+        private int level;
+        private int race;
+        private float xp;
+        private string name;
+
+        public void AssignPlayer(int level, int race, float xp, string name)
+        {
+            this.level = level;
+            this.race = race;
+            this.xp = xp;
+            this.name = name;
+        }
+    }
+}
